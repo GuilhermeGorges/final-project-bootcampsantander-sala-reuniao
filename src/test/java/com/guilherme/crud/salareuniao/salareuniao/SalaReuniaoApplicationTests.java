3c779fdc3433a50676125b6f05755b3cd87ab206
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2021-09-06 11:15:42 -0300

    First Commit

---
+++ src/test/java/com/guilherme/crud/salareuniao/salareuniao/SalaReuniaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.crud.salareuniao.salareuniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaReuniaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
