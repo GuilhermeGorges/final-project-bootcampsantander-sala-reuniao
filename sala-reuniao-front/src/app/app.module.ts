4777e05b0be813f10d4e7ccf739ad3e747af250d
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2021-09-08 11:34:33 -0300

    fixing implementation issues

--- sala-reuniao-front/src/app/app.module.ts
+++ sala-reuniao-front/src/app/app.module.ts
@@ -1,5 +1,8 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { CreateRoomComponent } from './create-room/create-room.component';
@@ -16,7 +19,10 @@ import { UpdateRoomComponent } from './update-room/update-room.component';
     UpdateRoomComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- sala-reuniao-front/src/app/create-room/create-room.component.ts
+++ sala-reuniao-front/src/app/create-room/create-room.component.ts
@@ -37,6 +37,6 @@ export class CreateRoomComponent implements OnInit {
   }
 
   goToList() {
-    this.router.navigate('/rooms')
+    this.router.navigate(['/rooms'])
   }
 }
